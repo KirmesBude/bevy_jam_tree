3bb893ff914947d97b605f06e08ac272daf42882
KirmesBude <kirmesbude@gmail.com>
2024-07-27 14:07:20 +0200

    Add back workflow

--- src/lib.rs
+++ src/lib.rs
@@ -36,7 +36,7 @@ impl Plugin for AppPlugin {
                 })
                 .set(WindowPlugin {
                     primary_window: Window {
-                        title: "Bevy Jam5".to_string(),
+                        title: "Bevy Jam5 Tree".to_string(),
                         canvas: Some("#bevy".to_string()),
                         fit_canvas_to_parent: true,
                         prevent_default_event_handling: true,
